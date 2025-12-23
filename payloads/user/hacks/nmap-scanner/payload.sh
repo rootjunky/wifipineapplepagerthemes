edbb72e812fc38f29c6f4b77686faff4d5dcb739
RootJunky <rootjunky@gmail.com>
2025-12-22 19:14:35 -0600

    Add spinner for Nmap scanning process

--- payloads/user/hacks/nmap-scanner/payload.sh
+++ payloads/user/hacks/nmap-scanner/payload.sh
@@ -7,6 +7,9 @@
 ip=$(IP_PICKER "Nmap scan enter IP" "192.168.1.1")
 LOG "$ip ready for scan"
 LOG "Scan in progress please wait"
+# The start spinner name IE Nmap-Scanning can not have any spaces in it or it will not stop. 
+id=$(START_SPINNER "Nmap-Scanning")
 ports=$(nmap --top-ports 100 "$ip")
+STOP_SPINNER $id
 LOG "$ports"
 LOG "Please scroll up to view results"
